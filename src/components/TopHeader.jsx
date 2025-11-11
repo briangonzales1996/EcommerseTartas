004eabb61726f31fc37c01e16daebbcfb5691e9b
briangonzales1996 <brianjose00@gmail.com>
2025-11-11 19:06:43 +0100

    logo header agragado

--- src/components/TopHeader.jsx
+++ src/components/TopHeader.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import maryPastel from "../../public/maryPastel.png"
+import logo from "../../public/caligrafia.png"
+
 function TopHeader() {
   return (
     <div>
         <div className='containerTopHeader'>
                 
                 <div className='containerLogoText'><img className='logoText' src={maryPastel} alt="logo pastel" /></div>
-
+                <div className='containerCaligrafia'><img src={logo} alt="tartas mary" /></div>
         </div>
         
     </div>
